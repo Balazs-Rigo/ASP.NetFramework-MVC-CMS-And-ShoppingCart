2f37e0d39e084b3909a6f47c4b6b18d3d664f4d3
Balazs Rigo <rigo.balazs86@gmail.com>
2021-10-24 11:27:33 +0200

    15. video megnezve. nagyon erdekes volt. a megjelenitett tablazat front-end rendezese drag and drop-pal. es adatbazisban a sorting mezo ennek megfeleloen update-elodik.

--- CMSShoppingCart/CMSShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CMSShoppingCart/CMSShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -50,14 +50,14 @@ namespace CMSShoppingCart.Areas.Admin.Controllers
 
                 //Init pageDTO
                 PageDTO dto = new PageDTO();
-                
+
                 //DTO title
                 dto.Title = model.Title;
 
                 //Check for and set slug if need be
                 if (string.IsNullOrEmpty(model.Slug))
                 {
-                    slug = model.Title.Replace(" ","-").ToLower();
+                    slug = model.Title.Replace(" ", "-").ToLower();
                 }
                 else
                 {
@@ -67,7 +67,7 @@ namespace CMSShoppingCart.Areas.Admin.Controllers
                 //Make sure title and slug are unique
                 if (db.Pages.Any(x => x.Title == model.Title) || db.Pages.Any(x => x.Slug == slug))
                 {
-                    ModelState.AddModelError("","That title os slug is already exist.");
+                    ModelState.AddModelError("", "That title os slug is already exist.");
                     return View(model);
                 }
 
@@ -101,7 +101,7 @@ namespace CMSShoppingCart.Areas.Admin.Controllers
             {
                 //Get the page
                 PageDTO dto = db.Pages.Find(id);
-                
+
                 //Confirmpage exist
                 if (dto == null)
                 {
@@ -145,17 +145,17 @@ namespace CMSShoppingCart.Areas.Admin.Controllers
                 {
                     if (string.IsNullOrEmpty(model.Slug))
                     {
-                        slug = model.Title.Replace(" ","-").ToLower();
+                        slug = model.Title.Replace(" ", "-").ToLower();
                     }
                     else
                     {
-                        slug = model.Slug.Replace(" ","-").ToLower();
+                        slug = model.Slug.Replace(" ", "-").ToLower();
                     }
                 }
 
                 //Make sure title and slug are unique
-                if (db.Pages.Where(x => x.Id!= id).Any(x=>x.Title == model.Title) ||
-                    db.Pages.Where(x=> x.Id != id).Any(x=> x.Slug == slug))
+                if (db.Pages.Where(x => x.Id != id).Any(x => x.Title == model.Title) ||
+                    db.Pages.Where(x => x.Id != id).Any(x => x.Slug == slug))
                 {
                     ModelState.AddModelError("", "That title or slug already exist!");
                     return View(model);
@@ -175,6 +175,65 @@ namespace CMSShoppingCart.Areas.Admin.Controllers
 
             //Redirect
             return RedirectToAction("EditPage");
+        }
+
+        // GET: Admin/Pages/PageDetails/id
+        [HttpGet]
+        public ActionResult PageDetails(int id)
+        {
+            PageVM model;
+
+            using (Db db = new Db())
+            {
+                PageDTO dto = db.Pages.Find(id);
+
+                if (dto == null)
+                {
+                    return Content("The page does not exist!");
+                }
+
+                model = new PageVM(dto);
+            }
+
+            return View(model);
+        }
+
+        // GET: Admin/Pages/DeletePage/id
+        [HttpGet]
+        public ActionResult DeletePage(int id)
+        {
+            using (Db db = new Db())
+            {
+                PageDTO dto = db.Pages.Find(id);
+
+                db.Pages.Remove(dto);
+
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        // POST: Admin/Pages/ReorderPages
+        [HttpPost]
+        public void ReorderPages(int[] id)
+        {
+            using (Db db = new Db())
+            {
+                int count = 1;
+
+                PageDTO dto;
+
+                foreach (var pageId in id)
+                {
+                    dto = db.Pages.Find(pageId);
+                    dto.Sorting = count;
+
+                    db.SaveChanges();
+
+                    count++;
+                }
+            }
         }
     }
 }
